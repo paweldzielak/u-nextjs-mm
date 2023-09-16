960b8aee0b8578450b0ca168092c089ccefb5add
Paweł Dziełak <pawel.programowanie@hotmail.com>
2023-09-16 08:20:32 +0200

    adding images to public folder

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -4,6 +4,9 @@ export default function StardewValleyPage() {
   return (
     <>
       <Heading text='Stardew valley' />
+
+      <img src="/images/stardew-valley.jpg" alt="" width={640} height={360} className="mb-2 rounded" />
+
     <p>
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima dolor nam cumque doloribus, ipsum adipisci aut tempore illum rem corrupti dolorum perspiciatis voluptatem dignissimos, magni error repudiandae, commodi impedit ipsam!
     </p>
